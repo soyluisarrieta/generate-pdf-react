444a1dd29cbb6d49d641d8eef9ad133f6607247e
Luis Arrieta <luisarrieta796@gmail.com>
2023-06-06 11:24:12 -0500

    Generar PDF y visualizarlo

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,66 @@
-import { Button } from 'bootstrap'
+import { useState } from 'react'
+
+import { Button } from 'react-bootstrap'
+import { PDFViewer } from '@react-pdf/renderer'
+
+import { Table } from './components/Table'
+import { TablePDF } from './components/TablePDF'
+
+const COLUMNS = [
+  'title',
+  'description',
+  'price',
+  'quantity'
+]
+
+const ROWS = [
+  [
+    'Tablet',
+    'Lorem ipsum dolor sit amet consectetur adipisicing elit. Quisquam, quidem.',
+    '$5000',
+    '3'
+  ],
+  [
+    'TV',
+    'Lorem1 ipsum dolor sit amet consectetur adipisicing elit. Quisquam, quidem.',
+    '$16000',
+    '1'
+  ],
+  [
+    'Portatil',
+    'Lorem2 ipsum dolor sit amet consectetur adipisicing elit. Quisquam, quidem.',
+    '$20000',
+    '2'
+  ],
+  [
+    'PC Desktop',
+    'Lorem3 ipsum dolor sit amet consectetur adipisicing elit. Quisquam, quidem.',
+    '$50000',
+    '1'
+  ]
+]
 
 function App () {
+  const [viewPDF, setViewPDF] = useState(false)
+
   return (
-    <div>
-      <h1>Hola mundo</h1>
-      <nav>
-        <Button>Ver Web</Button>
-        <Button>Ver PDF</Button>
-        <Button>Ver Descargar</Button>
+    <>
+      <nav className='navbar navbar-expand-lg navbar-light bg-light d-flex justify-content-center'>
+        <Button onClick={() => setViewPDF(!viewPDF)}>
+          {viewPDF ? 'Ocultar PDF' : 'Ver PDF'}
+        </Button>
       </nav>
-    </div>
+
+      {
+        viewPDF
+          ? (
+            <PDFViewer style={{ width: '100%', height: '90vh' }}>
+              <TablePDF columns={COLUMNS} rows={ROWS} />
+            </PDFViewer>
+            )
+          : <Table columns={COLUMNS} rows={ROWS} />
+      }
+    </>
   )
 }
 
